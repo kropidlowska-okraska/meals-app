bc0b68a8d076ee5eb3d3f7cfbd1d27b6e8ef11bc
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2025-03-18 15:32:33 +0100

    Add dynamic title to meals screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Platform } from "react-native";
+import { CATEGORIES } from "../data/dummyData";
 
 export default function Layout() {
 	return (
@@ -8,13 +9,25 @@ export default function Layout() {
 			<StatusBar style="light" />
 			<Stack
 				screenOptions={{
-                    headerStyle: { backgroundColor: "#351401" },
-                    headerTintColor: "white",
-                    contentStyle: { backgroundColor: "#3f2f25", paddingTop: Platform.OS === "ios" ? 16 : 32 },
+					headerStyle: { backgroundColor: "#351401" },
+					headerTintColor: "white",
+					contentStyle: {
+						backgroundColor: "#3f2f25",
+						paddingTop: Platform.OS === "ios" ? 16 : 32,
+					},
 				}}
 			>
-				<Stack.Screen name="index" options={{title: "All categories"}} />
-                <Stack.Screen name="[categoryId]/meals-overview" options={{title: "Meals Overview"}}/>
+				<Stack.Screen name="index" options={{ title: "All categories" }} />
+				<Stack.Screen
+					name="[categoryId]/meals-overview"
+					options={({ route }) => {
+						// @ts-ignore
+						const { categoryId } = route.params;
+						return {
+							title: CATEGORIES.find((cat) => cat.id === categoryId)?.title,
+						};
+					}}
+				/>
 			</Stack>
 		</>
 	);
